d24a4cdc8bb26f41f15285fd9d9f3707dc8da333
Elaine Zhang <elainez@mit.edu>
2018-03-04 23:20:39 -0500

    Delete files

--- background.js
+++
@@ -1,6 +0,0 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {
-    file: 'content.js'
-  });
-});
