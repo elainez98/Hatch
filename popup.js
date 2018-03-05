052485d3eba0287343001037d7ccd9d848a69857
adjaba <adjaba@mit.edu>
2018-03-05 16:39:58 -0500

    tasks

--- popup.js
+++ popup.js
@@ -3,7 +3,8 @@ $(function(){
 	$('#submit')
 		.click(function(){
 			console.log('button was clicked');
+			var text= $('#new-task').val();
 			$('ul')
-				.append('you clicked? \n');
+				.append(text+'\n');
 		})
 })
